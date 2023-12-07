66c9ae11bebfc0ccb86374f6a4e540a6176a4750
Pedro Nery <ppnery95@gmail.com>
2023-12-07 17:54:48 -0300

    adding header, title and public folder

---
+++ frontend/components/common/Title.tsx
@@ -0,0 +1,10 @@
+export default function Title({
+    children,
+    className,
+  }: {
+    children: string;
+    className?: string;
+  }) {
+    return <h1 className={`${className} text-3xl font-bold`}>{children}</h1>;
+  }
+  
